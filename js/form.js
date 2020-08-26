af7ca3591ebc9f4dd565fb9e33c5a1c0b67b753f
Gustavo Guimaraes <gustavoguimaraes18@gmail.com>
2020-08-26 16:16:50 -0300

    ugly code that works

--- js/form.js
+++ js/form.js
@@ -1,20 +1,70 @@
+firebase.initializeApp({
+  apiKey: 'AIzaSyD0qtFjQNMMclAZzEDu4iysSQP5I61uZPs',
+  authDomain: 'quero-hackday.firebaseapp.com',
+  projectId: 'quero-hackday'
+});
+
+const firestore = firebase.firestore();
+
+function query() {
+  firestore.collection('signatures')
+    .get()
+    .then(function (querySnapshot) {
+      querySnapshot.forEach(function (doc) {
+        console.log(doc.id, " => ", doc.data().name);
+      });
+    })
+}
+
+function create() {
+  const name = document.querySelector('input[name="name"]').value;
+  const email = document.querySelector('input[name="email"]').value;
+  const occupation = document.querySelector('input[name="occupation"]').value;
+  const createdAt = new Date();
+
+  firestore.collection("signatures").add({
+      name: name,
+      email: email,
+      occupation: occupation,
+      created_at: createdAt,
+    })
+    .then(function () {
+      console.log("Document successfully written!");
+    })
+    .catch(function (error) {
+      console.error("Error writing document: ", error);
+    });
+}
+
 document.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (grecaptcha.getResponse().length) {
-    const form = e.target;
-    const url = '';
+    create();
+  }
+});
 
-    fetch(url, {
-        method: 'POST',
-        body: new FormData(form)
-      })
-      .then(doc => {
-        alert('Sucesso');
 
-      })
-      .catch(() => {
-        alert('Erro');
-      });
+document.getElementById('signButton').addEventListener('click', function () {
+  document.getElementById('mask').classList.add('active');
+  document.body.classList.add('body-with-modal');
+});
+
+function closeModal() {
+ document.getElementById('mask').classList.remove("active");
+ document.body.classList.remove('body-with-modal');
+}
+
+document.getElementById('close').addEventListener('click', function () {
+  closeModal();
+});
+
+document.getElementById('mask').addEventListener('click', function () {
+  closeModal();
+});
+
+document.addEventListener("keyup", event => {
+  if (event.keyCode === 27) {
+    closeModal();
   }
 });
