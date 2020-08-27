5b9519086f90f92abebe1deff77cee4948999bab
Gustavo Guimaraes <gustavoguimaraes18@gmail.com>
2020-08-26 23:50:29 -0300

    remove console log

--- js/media.js
+++ js/media.js
@@ -1,25 +1,3 @@
-function queryImages() {
-  firestore.collection('assets')
-    .where("kind", "==", "image")
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        console.log(doc.id, " => ", doc.data().name, "and", doc.data().path, "and", doc.data().kind);
-      });
-    })
-}
-
-function queryVideos() {
-  firestore.collection('assets')
-    .where("kind", "==", "video")
-    .get()
-    .then(function(querySnapshot) {
-      querySnapshot.forEach(function(doc) {
-        console.log(doc.id, " => ", doc.data().name, "and", doc.data().path, "and", doc.data().kind);
-      });
-    })
-}
-
 function queryPartners() {
   firestore.collection('assets')
     .where("kind", "==", "partner")
--- js/testimonials.js
+++ js/testimonials.js
@@ -16,25 +16,4 @@ function queryApprovedTestimony() {
     })
 }
 
-function createTestimony() {
-  const name = document.querySelector('.author-name').value;
-  const testimony = document.querySelector('.testimony').value;
-  const email = document.querySelector('.author-email').value;
-  const createdAt = new Date();
-
-  firestore.collection("testimony").add({
-      testimony: testimony,
-      name: name,
-      email: email,
-      approved: false,
-      created_at: createdAt
-    })
-    .then(function () {
-      console.log("Testimony successfully written!");
-    })
-    .catch(function (error) {
-      console.error("Error writing testimony: ", error);
-    });
-}
-
 queryApprovedTestimony();
